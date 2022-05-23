3221136d7141d54fc998af886240c6678b1a88ad
Ruslan <ruslan.shtybaev@gmail.com>
2022-05-23 22:50:09 +0300

    Initial Commit

---
+++ MVVMExersice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMExersice
+//
+//  Created by Руслан Штыбаев on 23.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
